0d11adb2e7c3a623ec3f12294b96e70efbf3ddc4
Deepak Katariya <deepak.sorthiya@gmail.com>
2024-03-02 22:48:27 +0530

    upgrade to spring boot 3.2.3

--- src/main/java/com/example/SpringBoot3PrometheusApplication.java
+++ src/main/java/com/example/SpringBoot3PrometheusApplication.java
@@ -1,12 +1,25 @@
 package com.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBoot3PrometheusApplication {
+
+    private final Logger logger = LoggerFactory.getLogger(SpringBoot3PrometheusApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBoot3PrometheusApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        logger.info("log message {} ", "hello");
+        return "Hello";
     }
 }
